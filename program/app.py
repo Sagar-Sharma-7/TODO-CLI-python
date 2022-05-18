ec734c117abb8a66c1681a1936eae78d7abf82a1
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-05-18 16:28:14 +0530

    first commit - creating new to do list

--- program/app.py
+++ program/app.py
@@ -1,8 +1,49 @@
 import pickle
 import datetime as dt
-# f = open("./files/to_do_list.dat", "ab")
-x = dt.datetime.now()
-print(x.strftime("%B"))
 
+# colors for terminal
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+f = open("./files/to_do_list.dat", "ab")
+
+print("")
+print(bcolors.OKBLUE + "Starting program...\n")
+options = ["Create new list", "Read existing list", "Delete pervious list"]
+
+def to_do():
+    ## Presenting options to user
+    for i in range(0,3):
+        print("\t",i + 1, " --- ", options[i])
+    # option selected
+    option = int(input("Enter your option number: "))
+    if option == 1:
+        ## creating new list
+        print("")
+        print(bcolors.WARNING + "Creating new TO DO list...")
+        d = dt.datetime.now()
+        n = 1
+        t_list = [d]
+        while True:
+            print(bcolors.BOLD + "Enter task no ", n)
+            task = input("\tEnter your task: ")
+            t_list.append(task)
+            ans = input("Do you wish to enter more tasks (y/n)? : ")
+            if ans.lower() in ["n", "no", "nope", "nah"]:
+                print("Adding tasks...")
+                break
+        print(t_list)
+        
+to_do()
+    
 
 
